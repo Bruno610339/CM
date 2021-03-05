c228c25b19e1cc1b75b58ecdcf362e51796e7b45
Bruno610339 <bmiguelgomes@ipvc.pt>
2021-03-05 15:20:57 +0000

    Alteracao 1

---
+++ app/src/main/java/ipvc/estg/teste_android_git/MainActivity.kt
@@ -0,0 +1,11 @@
+package ipvc.estg.teste_android_git
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
